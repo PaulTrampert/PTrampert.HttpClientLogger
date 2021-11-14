2441169960e3f64162fbbfed404dcaed6948ce01
Paul Trampert <paul.trampert@ptrampert.com>
2021-11-14 01:58:54 -0500

    Implement the actual logger.

--- PTrampert.HttpClientLogger/HttpClientLogger.cs
+++ PTrampert.HttpClientLogger/HttpClientLogger.cs
@@ -3,30 +3,44 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace PTrampert.HttpClientLogger
 {
     public class HttpClientLogger : DelegatingHandler
     {
         private ILogger log;
+        private LogMessageBuilder builder;
+        private HttpClientLoggerConfig config;
 
-        public HttpClientLogger(ILoggerFactory logFactory, HttpMessageHandler innerHandler = null) 
-            : this(logFactory.CreateLogger<HttpClientLogger>(), innerHandler) 
+        public HttpClientLogger(ILoggerFactory logFactory, IOptionsSnapshot<HttpClientLoggerConfig> config, HttpMessageHandler innerHandler = null) 
+            : this(logFactory.CreateLogger<HttpClientLogger>(), config, innerHandler) 
         {
         }
         
-        public HttpClientLogger(ILogger log, HttpMessageHandler innerHandler = null)
+        public HttpClientLogger(ILogger log, IOptionsSnapshot<HttpClientLoggerConfig> config, HttpMessageHandler innerHandler = null)
             : base(innerHandler ?? new HttpClientHandler())
         {
             this.log = log;
+            this.config = config.Value;
+            builder = new LogMessageBuilder(this.config.PrivateFields, this.config.PrivateHeaders);
         }
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            
-            var response = await base.SendAsync(request, cancellationToken);
-            log.LogInformation("Got HTTP Response");
-            return response;
+            try
+            {
+                var response = await base.SendAsync(request, cancellationToken);
+                var requestLogMsg = builder.BuildRequestLogMessage(request);
+                var responseLogMsg = builder.BuildResponseLogMessage(response);
+                log.Log(config.LogLevel, "Request: {@Request} Response: {@Response}", requestLogMsg, responseLogMsg);
+                return response;
+            } 
+            catch (Exception e)
+            {
+                log.LogError(e, "Error sending request: {@Request}", await builder.BuildRequestLogMessage(request));
+                throw e;
+            }
         }
     }
 }
---
+++ PTrampert.HttpClientLogger/HttpClientLoggerConfig.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PTrampert.HttpClientLogger
+{
+    public class HttpClientLoggerConfig
+    {
+        public LogLevel LogLevel { get; set; } = LogLevel.Information;
+
+        public PrivateFields PrivateFields { get; set; } = new PrivateFields();
+
+        public PrivateHeaders PrivateHeaders { get; set; } = new PrivateHeaders();
+    }
+}
